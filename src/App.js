a5687ffc4ce7e5dd45c7cfb97c6bc504ce86dc79
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2021-12-15 20:41:38 +0100

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return <Box>Chololate city studio</Box>;
+}
+
+export default App;
